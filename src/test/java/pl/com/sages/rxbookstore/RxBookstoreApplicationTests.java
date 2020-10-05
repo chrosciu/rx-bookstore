947d43394cb00910b57a84d86b2701f7234a3363
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-10-05 22:58:07 +0200

    basic integration test

--- src/test/java/pl/com/sages/rxbookstore/RxBookstoreApplicationTests.java
+++ src/test/java/pl/com/sages/rxbookstore/RxBookstoreApplicationTests.java
@@ -1,13 +1,22 @@
 package pl.com.sages.rxbookstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class RxBookstoreApplicationTests {
+	@Autowired
+	private BookService bookService;
 
 	@Test
-	void contextLoads() {
+	void getBooksTest() {
+		StepVerifier.create(bookService.getBooks())
+				.assertNext(book -> assertEquals("Dziady", book.getTitle()))
+				.verifyComplete();
 	}
 
 }
